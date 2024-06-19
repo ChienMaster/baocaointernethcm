419a864ef5cdbc2a987a8902eaeb2d4d1127ee94
ChienMaster <131136108+ChienMaster@users.noreply.github.com>
2024-06-19 13:51:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,14 @@ with tabs[0]:
     selected_project = st.selectbox('Chọn dự án', project_options)
 
     if selected_project:
+        options = ['Config', 'Check', 'Clear', 'Reboot', 'Bridge', 'thoai', 'iptv']
         st.write(f'Bạn đã chọn dự án: {selected_project}')
         st.write('Đang chuyển đến giao diện cấu hình OLT...')
-        # TODO: Thêm logic để chuyển đến giao diện cấu hình OLT
+        congthuc = st.text_area('Nhập công thức theo hướng dẫn của Chiến ACOM (Nếu không biết vui lòng liên hệ: 0932277923 Phone/Zalo Chiến ACOM)')
+        if selected_project == 'Vinhomes Grand Park PK2':
+            choice = st.selectbox('Bạn muốn gì?', options)
+            if choice:
+                st.write('Bạn muốn {} OLT Vinhomes Grand Park PK2'.format(choice))
 
 with tabs[1]:
     recent_ticket_number = int(max(st.session_state.df.ID).split('-')[1])
