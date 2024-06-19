49eb466b6e8b1e34f246713bb234b451f6a677dd
ChienMaster <131136108+ChienMaster@users.noreply.github.com>
2024-06-19 14:13:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,24 +14,6 @@ id_values = ['TICKET-{}'.format(i) for i in range(1000, 1100)]
 issue_list = [generate_issue() for _ in range(100)]
 
 
-def generate_random_dates(start_date, end_date, id_values):
-    date_range = pd.date_range(start_date, end_date).strftime('%m-%d-%Y')
-    return np.random.choice(date_range, size=len(id_values), replace=False)
-
-## Generate 100 rows of data
-data = {'Issue': issue_list,
-        'Status': np.random.choice(['Open', 'In Progress', 'Closed'], size=100),
-        'Priority': np.random.choice(['High', 'Medium', 'Low'], size=100),
-        'Date Submitted': generate_random_dates(start_date, end_date, id_values)
-    }
-df = pd.DataFrame(data)
-df.insert(0, 'ID', id_values)
-df = df.sort_values(by=['Status', 'ID'], ascending=[False, False])
-
-## Create DataFrame
-if 'df' not in st.session_state:
-    st.session_state.df = df
-
 
 
 # Main content
